1bc8d13b2fabc63eb69791e7e137c55fa577fbe6
Amogh Anakru <amoghanakru@gmail.com>
2018-03-03 14:37:41 -0500

    Modified classes

--- app/src/main/java/com/sleep/sleepapp/patientInfo.java
+++ app/src/main/java/com/sleep/sleepapp/patientInfo.java
@@ -4,33 +4,5 @@ package com.sleep.sleepapp;
  * Created by 2718281828 on 3/3/18.
  */
 
-
-
-public class patientInfo {
-    static private String name; //Legal name
-    static private String sex = "m"; //"m" or "f"
-    static private double weight; //weight in pounds
-    static private double height; //height in inches
-    static private SleepInfo sInfo; //stores Sleep Info
-
-    private patientInfo(){
-
-    }
-
-    public static double getWeight(){
-        return weight;
-    }
-    public static double getHeight(){
-        return height;
-    }
-    public static void setWeight(double w){
-        if(w>0){weight=w;}
-    }
-    public static void setHeight(double h){
-        if(h>0){height=h;}
-    }
-
-
-
-
+public class PatientInfo {
 }
